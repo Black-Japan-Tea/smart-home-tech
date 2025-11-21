7eda52682426b6a682bbb7755ad30c17e886d2c5
Black-Japan-Tea <mister.garry75@yandex.ru>
2025-11-21 15:08:13 +0300

    bugfix

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/controller/GrpcEventController.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/controller/GrpcEventController.java
@@ -1,1 +1,61 @@
+package ru.yandex.practicum.kafka.telemetry.controller;
 
+import com.google.protobuf.Empty;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+import ru.yandex.practicum.grpc.telemetry.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.SensorEventProto;
+import ru.yandex.practicum.kafka.telemetry.mapper.ProtobufMapper;
+import ru.yandex.practicum.kafka.telemetry.service.CollectorService;
+import telemetry.services.CollectorControllerGrpc;
+
+@Slf4j
+@GrpcService
+@RequiredArgsConstructor
+public class GrpcEventController extends CollectorControllerGrpc.CollectorControllerImplBase {
+
+    private final ProtobufMapper protobufMapper;
+    private final CollectorService collectorService;
+
+    @Override
+    public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
+        try {
+            log.debug("Received sensor event via gRPC: {}", request.getId());
+            var dto = protobufMapper.toDto(request);
+            collectorService.collectSensorEvent(dto);
+            
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Error processing sensor event: {}", request.getId(), e);
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+
+    @Override
+    public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
+        try {
+            log.debug("Received hub event via gRPC: {}", request.getHubId());
+            var dto = protobufMapper.toDto(request);
+            collectorService.collectHubEvent(dto);
+            
+            responseObserver.onNext(Empty.getDefaultInstance());
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Error processing hub event: {}", request.getHubId(), e);
+            responseObserver.onError(new StatusRuntimeException(
+                    Status.INTERNAL
+                            .withDescription(e.getLocalizedMessage())
+                            .withCause(e)
+            ));
+        }
+    }
+}
